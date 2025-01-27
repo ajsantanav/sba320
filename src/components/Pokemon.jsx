16163b3abf0bb44992b054a45a33bd3b79156d2a
ajsantanav <adriansantana.v@gmail.com>
2025-01-27 10:37:21 -0500

    I dont know the problme to this

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -8,17 +8,17 @@ function Pokemon({ pokemon }) {
     <div className='pokemon-container'>
       {pokemon.map(({id, pokeName, spriteShiny, spriteNormal, type1, type2})) => (
           <div key={id} className='pokemon-card'>
-          <div>
-            <span className='number'>#{id} {pokeName}</span>
+            <div>
+              <span className='number'>#{id} {pokeName}</span>
+            </div>
+            <div className='imageContainer'>
+              <img src={spriteNormal} alt={pokeName} />
+            </div>
+            <div className='type'>
+                <span className='typeSpan'>{type1}</span>
+                {type2 && <span className='typeSpan'>{type2}</span>}
+            </div>
           </div>
-          <div className='imageContainer'>
-            <img src={spriteNormal} alt={pokeName} />
-          </div>
-          <div className='type'>
-              <span className='typeSpan'>{type1}</span>
-              {type2 && <span className='typeSpan'>{type2}</span>}
-          </div>
-        </div>
         )}
     </div>
   );
